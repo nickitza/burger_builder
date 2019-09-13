a374ef47c4049b122edb6dc74ce4adf3e0335710
Nicki Nixon <nickitza@gmail.com>
2019-09-13 15:40:27 -0600

    create proptypes, burger skeleton

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './BurgerIngredient.module.css'
+import PropTypes from 'prop-types'
 
 const burgerIngredient = (props) =>{
   let ingredient = null;
@@ -32,6 +33,10 @@ const burgerIngredient = (props) =>{
       ingredient= null
 
   }
+}
+
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
 }
 
 export default burgerIngredient
