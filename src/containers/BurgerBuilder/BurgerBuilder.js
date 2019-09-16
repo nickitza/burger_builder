f616cde2704bcade35d57e4c3ffe2d7125b3788c
Nicki Nixon <nickitza@gmail.com>
2019-09-16 11:32:01 -0600

    turn state object into array

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,16 +5,16 @@ import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends React.Component{
   state = {
     ingredients: {
-      lettuce: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      lettuce: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   }
   render(){
     return(
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     )
