abea11fb326af5e7398cff9823a60af7157907d9
Nicki Nixon <nickitza@gmail.com>
2019-09-16 11:16:39 -0600

    change ingredient styling

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,6 +6,8 @@ const burger = (props) => {
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="lettuce" />
+      <BurgerIngredient type="bacon" />
       <BurgerIngredient type="cheese" />
       <BurgerIngredient type="meat" />
       <BurgerIngredient type="bread-bottom" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
-
+  
 class BurgerBuilder extends React.Component{
   render(){
     return(
