7b50b83b1340f36bf0e3aa997598156cc9b5444f
Nicki Nixon <nickitza@gmail.com>
2019-09-16 11:18:34 -0600

    add state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
-  
+
 class BurgerBuilder extends React.Component{
+  state = {
+    ingredients: {
+      lettuce: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  }
   render(){
     return(
       <Aux>
